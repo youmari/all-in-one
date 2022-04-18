3a290df4818a5d4805011e45723d56e09709611a
youmari <youmari300@gmail.com>
2022-04-18 15:30:28 +0000

    Add to authentication needed  to group and expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,6 @@
 class ExpensesController < ApplicationController
   before_action :set_expense, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /expenses or /expenses.json
   def index
