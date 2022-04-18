a6f753f2a7bd51a466bc25f8cd88207320a7affa
youmari <youmari300@gmail.com>
2022-04-18 15:32:23 +0000

    Change user_id to author_id in expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -66,6 +66,6 @@ class ExpensesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def expense_params
-      params.require(:expense).permit(:name, :amount, :user_id)
+      params.require(:expense).permit(:name, :amount, :author_id)
     end
 end
