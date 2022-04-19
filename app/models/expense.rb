5cb2cf15f24b3bbaf5b22098e30980a608c4c74d
youmari <youmari300@gmail.com>
2022-04-19 07:36:17 +0000

    Add home page

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :groups
   validates :name, presence: true, length: {maximum: 50}
   validates :amount, presence: true, comparison: { greater_than_or_equal_to: 0 }
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,7 @@
 class Group < ApplicationRecord
+  has_one_attached :icon
   belongs_to :user
   validates :name, presence: true, length: {maximum: 50}
-  validates :icon, presence: true,
-  
-  
-  has_many :expenses
+  validates :icon, presence: true
+  has_many :expenses, foreign_key: :author_id
 end
