cdb2d083d7cf63d51fb8fe6b5246c3247c9a2e16
youmari <youmari300@gmail.com>
2022-04-19 17:53:35 +0000

    Add relationship many to many

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :groups, through :categories
+  has_many :categories
+  has_many :groups, through: :categories, dependent: :destroy
   validates :name, presence: true, length: {maximum: 50}
   validates :amount, presence: true, comparison: { greater_than_or_equal_to: 0 }
 end
