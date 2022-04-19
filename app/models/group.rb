289ff19d9736a175b048b85132734d7190b8f541
youmari <youmari300@gmail.com>
2022-04-19 18:00:26 +0000

    Add method to count total of amounts and dependents

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,9 @@ class Group < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, length: {maximum: 50}
   validates :icon, presence: true
-  has_many :expenses, foreign_key: :author_id, through :categories
+  has_many :categories, dependent: :destroy
+  has_many :expenses, through: :categories, dependent: :destroy
+
+  scope :total, -> (group) { group.expenses.includes(:groups).sum(:amount)}
+  # Ex:- scope :active, -> {where(:active => true)}
 end
