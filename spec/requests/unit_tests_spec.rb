d37f1cdec7eccb065fde09eae6c44c1f41e201a9
youmari <youmari300@gmail.com>
2022-04-21 06:15:23 +0000

    Fix rubocop

--- spec/requests/unit_tests_spec.rb
+++ spec/requests/unit_tests_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Expenses', type: :request do
-  before(:each) do 
+  before(:each) do
     @user = User.create(name: 'lolo', email: 'test@gmail.com', password: 1_234_567, password_confirmation: 1_234_567)
     sign_in @user
   end
@@ -18,9 +18,9 @@ RSpec.describe 'Expenses', type: :request do
     end
   end
   describe 'GET /show' do
-    before(:each) do 
-      @group = Group.create(name:'test1',user_id:@user.id)
-      @group.icon.attach(io: File.open("./spec/features/search.png"), filename: "search.png", content_type: "image/png")
+    before(:each) do
+      @group = Group.create(name: 'test1', user_id: @user.id)
+      @group.icon.attach(io: File.open('./spec/features/search.png'), filename: 'search.png', content_type: 'image/png')
       @group.save!
       get group_path(@group.id)
     end
@@ -30,10 +30,10 @@ RSpec.describe 'Expenses', type: :request do
     it 'should the body has test1 text' do
       expect(response.body).to include('test1')
     end
-     it 'should the body has expense' do
+    it 'should the body has expense' do
       expect(response.body).to include('Expense')
     end
-       it 'should the body has ADD Expense' do
+    it 'should the body has ADD Expense' do
       expect(response.body).to include('ADD EXPENSE')
     end
     it 'should render index template' do
